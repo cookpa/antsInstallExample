fe79c8fd1d35fb7cb4b5f1b6e1d25728d9210184
Philip A Cook <cookpa@pennmedicine.upenn.edu>
2019-11-18 14:24:40 -0500

    Adding install script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # antsInstallExample
-Simple bash script that downloads and installs ANTs in a unix environment
+Simple bash script that downloads and installs ANTs in a unix environment.
+
+See
+
+  https://github.com/ANTsX/ANTs/wiki/Compiling-ANTs-on-Linux-and-Mac-OS
+
+for more information. 
+
---
+++ installANTs.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+antsBuildInstructions="https://github.com/ANTsX/ANTs/wiki/Compiling-ANTs-on-Linux-and-Mac-OS"
+
+echo "
+This script will download ANTs, build and install under the current directory
+
+Developer tools including compilers, git and cmake must be installed
+
+If you encounter errors, please see the installation instructions at
+
+  $antsBuildInstructions
+
+Build will proceed in 5 seconds
+"
+
+sleep 5
+
+workingDir=${PWD}
+
+# Clone the repo
+git clone https://github.com/ANTsX/ANTs.git
+
+# If you want to build a particular release, do so here
+# cd ANTs
+# git checkout v2.3.1
+# cd -
+
+# Number of threads used by make
+buildThreads=4
+
+# Where to build, should be an empty directory
+buildDir=${workingDir}/build
+installDir=${workingDir}/install
+
+mkdir $buildDir $installDir
+
+cd $buildDir
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$installDir \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DSuperBuild_ANTS_USE_GIT_PROTOCOL=OFF \
+    -DBUILD_TESTING=OFF \
+    -DRUN_LONG_TESTS=OFF \
+    -DRUN_SHORT_TESTS=OFF \
+    ${workingDir}/ANTs 2>&1 | tee cmake.log
+
+if [[ $? -ne 0 ]]; then
+  echo "ANTs SuperBuild configuration failed. Please review documentation at
+
+    $antsBuildInstructions
+
+  If opening an issue, please attach
+  
+  ${buildDir}/cmake.log
+  ${buildDir}/CMakeCache.txt
+  ${buildDir}/CMakeFiles/CMakeError.log
+  ${buildDir}/CMakeFiles/CMakeOutput.log
+  
+"
+  exit 1 
+fi
+
+make -j $buildThreads 2>&1 | tee build.log
+
+if [[ ! -f "CMakeFiles/ANTS-complete" ]]; then
+  echo "ANTs compilation failed. Please review documentation at
+
+    $antsBuildInstructions
+
+  If opening an issue, please attach
+
+  ${buildDir}/build.log
+  ${buildDir}/cmake.log
+  ${buildDir}/CMakeCache.txt
+  ${buildDir}/CMakeFiles/CMakeError.log
+  ${buildDir}/CMakeFiles/CMakeOutput.log
+  
+"
+  exit 1
+fi
+
+cd ANTS-build
+make install 2>&1 | tee install.log
+
+antsRegExe="${installDir}/bin/antsRegistration"
+
+if [[ ! -f ${antsRegExe} ]]; then
+  echo "Installation failed. Please review documentation at
+
+    $antsBuildInstructions
+
+  If opening an issue, please attach
+
+  ${buildDir}/build.log
+  ${buildDir}/cmake.log
+  ${buildDir}/CMakeCache.txt
+  ${buildDir}/ANTS-build/install.log
+  ${buildDir}/CMakeFiles/CMakeError.log
+  ${buildDir}/CMakeFiles/CMakeOutput.log
+
+"
+  exit 1
+fi
+
+echo "Installation complete, running ${antsRegExe}"
+
+${antsRegExe} --version
+
